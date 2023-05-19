dff9762f9093f71b5f911c434829371ff7720928
campossalex <32500181+campossalex@users.noreply.github.com>
2023-05-19 19:23:41 +0200

    Update DE-telco_data.py

--- resources/DE-telco_data.py
+++ resources/DE-telco_data.py
@@ -1,3 +1,7 @@
+########################################
+## Should use Spark 3 to run this job ##
+########################################
+
 from pyspark.sql import SparkSession
 from datetime import datetime
 import sys
@@ -7,9 +11,10 @@ config = configparser.ConfigParser()
 config.read('/app/mount/parameters.conf')
 data_lake_name=config.get("general","data_lake_name")
 s3BucketName=config.get("general","s3BucketName")
-username=config.get("general","username")
 
-print("Running as Username: ", username)
+## send this argument in the job configuration
+username=sys.argv[1]
+print("Running job as Username: ", username)
 
 spark = SparkSession \
     .builder \
@@ -21,12 +26,12 @@ spark = SparkSession \
     .getOrCreate()
 
 #---------------------------------------------------
-#               INSERT DATA
+#               TRUNCATE TABLE
 #---------------------------------------------------
-
-# INSERT DATA - APPEND FROM DATAFRAME
-
 spark.sql("TRUNCATE TABLE spark_catalog.{}.telco_data_curated".format(username))
 
+#---------------------------------------------------
+#               INSERT DATA
+#---------------------------------------------------
 temp_df = spark.sql("SELECT multiplelines, paperlessbilling, d.description as gender, onlinesecurity, internetservice, techsupport, b.description as contract, churn, seniorcitizen, deviceprotection, streamingtv, streamingmovies, totalcharges, c.description as partner, monthlycharges, customerid, e.description as dependents, onlinebackup, phoneservice, tenure, paymentmethod FROM {}.telco_iceberg_kafka a JOIN master_data.contract b ON b.id = contract JOIN master_data.misc c ON c.id = partner JOIN master_data.misc d ON d.id = gender JOIN master_data.misc e ON e.id = dependents".format(username))
 temp_df.writeTo("spark_catalog.{}.telco_data_curated".format(username)).append()
