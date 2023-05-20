04aec0eac4b2c6ce7e2e52950a5126bc8b2beecb
campossalex <32500181+campossalex@users.noreply.github.com>
2023-05-20 23:18:58 +0200

    Update CDE-updateTable.py

--- resources/CDE-updateTable.py
+++ resources/CDE-updateTable.py
@@ -10,7 +10,6 @@ config.read('/app/mount/parameters.conf')
 data_lake_name=config.get("general","data_lake_name")
 s3BucketName=config.get("general","s3BucketName")
 
-
 spark = SparkSession \
     .builder \
     .appName("ICEBERG LOAD") \
@@ -19,32 +18,24 @@ spark = SparkSession \
     .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
     .config("spark.yarn.access.hadoopFileSystems", data_lake_name)\
     .getOrCreate()
+
 DATABASE=sys.argv[1]
-
-
-print("Running as Username: ", DATABASE)
-
-
+print("Running job as Username: ", DATABASE)
 
 #CREATE EXTERNAL TABLE ext_t1 STORED AS ICEBERG TBLPROPERTIES ('iceberg.table_identifier'='default.telco_iceberg');
 
 from pyspark.sql.functions import rand,abs,when,regexp_replace
 print("spark_catalog."+ DATABASE + ".telco_data_curated")
 df=spark.read.format("iceberg").load("spark_catalog."+ DATABASE + ".telco_data_curated")
-#df = df.withColumn("tenure", df["tenure"].cast(DoubleType()))
-#df = df.withColumn("monthlycharges", df["monthlycåharges"].cast(DoubleType()))
-#df = df.withColumn("totalcharges", df["totalcharges"].cast(DoubleType()))
 
 df=df.select('customerid','monthlycharges','churn').where(df.churn != 'Yes')
 df=df.drop_duplicates()
 df=df.drop_duplicates(['customerid'])
-df=df.withColumn('monthlycharges', regexp_replace('monthlycharges', ',', '.'))
 df=df.withColumn("monthlycharges",df.monthlycharges.cast(DoubleType()))
 
 df=df.withColumn("monthlycharges",df["monthlycharges"]+df["monthlycharges"]*(2*rand()-1))
 #print(df.head(10))
-df=df.withColumn("monthlycharges",df.monthlycharges.cast(StringType()))
-df=df.withColumn('monthlycharges', regexp_replace('monthlycharges', r'[.]', ','))
+df=df.withColumn("monthlycharges",df.monthlycharges)
 
 #df['monthlycharges']=str(df['monthlycharges'])
 #df = df.withColumn('churn', when(rand() > 0.1, df['churn']).otherwise('Yes'))
